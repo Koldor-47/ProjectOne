83dd3b25aeb3d1849ab8162187874dcc0934df6b
Nick <nickhowlett13@gmail.com>
2022-01-03 23:03:16 +0800

    Hello World cpp

--- Cpp/Learning/HelloWorld/main.cpp
+++ Cpp/Learning/HelloWorld/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <random>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    std::vector<std::string> names {"nick", "Penny", "Bri", "Maggie", "jonh"};
+    std::random_device random_device;
+    std::mt19937 engine{random_device()};
+
+    std::uniform_int_distribution<int> dist(0, names.size()-1);
+    
+    std::cout << "Hello, " << names[dist(engine)] << "!\n" << std::endl;
 }
